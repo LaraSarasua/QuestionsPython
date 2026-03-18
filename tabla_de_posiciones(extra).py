99c13c5d6276e2f4a9494ab253258d3b15670e9e
LaraSarasua <lara.sarasua05@gmail.com>
2026-03-18 15:08:27 -0300

    Se agregaron casi todas las funcionalidades menos la de mostrar tabla (case 3)

---
+++ tabla_de_posiciones(extra).py
@@ -0,0 +1,71 @@
+equipos = {} #diccionario asi cada equipo tiene un puntaje "Equipo1 : 6 pts"
+
+print("--------- Bienvenido al torneo de futbol ---------")
+
+print() 
+
+while True:
+    print("1. Agregar equipo")
+    print("2. Registrar resultado")
+    print("3. Mostrar tabla")
+    print("4. Eliminar un equipo")
+    print("5. Finalizar")
+
+    print()
+    opcion = input("Seleccione una opcion: ")
+
+    match opcion:
+        case "1":
+            print("Eligió la opcion 1 a continuacion ingrese la informacion requerida.")
+            nombre = input("Ingrese el nomre del equipo: ")
+            if(nombre in equipos):
+                print("El equipo ya existe. Por favor ingrese uno nuevo.")
+                continue
+            else:
+                equipos[nombre] = 0
+                print("El equipo fue agregado con exito...")
+        case "2":
+            print("Eligió la opcion 2 a continuacion ingrese la informacion requerida.")
+            local = input("Equipo local: ")
+            visitante = input("Equipo visitante: ")
+            marcador = input("Ingrese marcador(ej : 4-2): ")
+
+            if local not in equipos or visitante not in equipos:
+                print("Uno de los equipos no existe.")
+                continue
+
+            if "-" not in marcador:
+                print("Formato no valido")
+                continue
+            
+            goles = marcador.split("-")
+
+            goles_local = int(goles[0])
+            goles_visitante = int(goles[1])
+            
+            if(goles_local > goles_visitante):
+                equipos[local] += 3
+            elif (goles_local < goles_visitante):
+                equipos[visitante] += 3
+            else:
+                equipos[local] += 1
+                equipos[visitante] += 1
+
+        #case "3":
+            # ordenados = #preguntar no se como hacer
+
+        case "4":
+            nombre = input("Que equipo desea eliminar? ")
+
+            if(nombre in equipos):
+                del equipos[nombre]
+                print("Se ha eliminado al equipo ingresado")
+            else:
+                print("El equipo ingresado no existe")
+        case "5":
+            print("Finalizacion del programa....")
+            break
+        
+        case _:
+            print("Opcion Invalida")
+        
