abc4ffe60dbbbf7beb5f8fab8f574ed74bd14c3a
LaraSarasua <lara.sarasua05@gmail.com>
2026-03-18 13:19:47 -0300

    Se agregó el ultimo punto de poder jugar por categoria sin repetir las palabras

--- questions.py
+++ questions.py
@@ -32,9 +32,6 @@ categorias = {
     "bandas de musica" : bandas,
     "paises" : paises
 }
-
-guessed = []
-attempts = 6
 score = 0
 
 print("¡Bienvenido al Ahorcado!")
@@ -47,49 +44,56 @@ for clave in categorias:
 print()
 
 categoria_elegida = input("Elegi la categoria con la que queres jugar: ")
+palabras = random.sample(categorias[categoria_elegida], len(categorias[categoria_elegida]))
 
-word = random.choice(categorias[categoria_elegida])
+for word in palabras:
 
-while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
-        if letter in guessed:
-            progress += letter + " "
-        else:
-            progress += "_ "
-    
-    print(progress)
-    
-    # Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        print("¡Ganaste!")
-        score += 6
-        break
-    
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
-    
-    letter = input("Ingresá una letra: ")
+    guessed = []
+    attempts = 6
 
-    if len(letter) != 1 or not letter.isalpha(): #la funcion isalpha en python verifica q lo que este dentro de la variable sea si o si una letra del abecedario. 
-        print("Entrada incorrecta. Solamente se aceptan letras del abecedario")
-        print()
-        continue #para que vuelva a "entrar" en el while para que pregunte otra letra ya que la ingresada no fue valida.
-
-    if letter in guessed:
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
-    else:
-        guessed.append(letter)
-        attempts -= 1
-        score -= 1
-        print("Esa letra no está en la palabra.")
+    print(f"Nueva palabra de la categoría {categoria_elegida}")
     print()
-else:
-    print(f"¡Perdiste! La palabra era: {word}")
-    score = 0
+
+    while attempts > 0:
+        # Mostrar progreso: letras adivinadas y guiones para las que faltan
+        progress = ""
+        for letter in word:
+            if letter in guessed:
+                progress += letter + " "
+            else:
+                progress += "_ "
+        
+        print(progress)
+        
+        # Verificar si el jugador ya adivinó la palabra completa
+        if "_" not in progress:
+            print("¡Ganaste!")
+            score += 6
+            break
+        
+        print(f"Intentos restantes: {attempts}")
+        print(f"Letras usadas: {', '.join(guessed)}")
+        
+        letter = input("Ingresá una letra: ")
+
+        if len(letter) != 1 or not letter.isalpha(): #la funcion isalpha en python verifica q lo que este dentro de la variable sea si o si una letra del abecedario. 
+            print("Entrada incorrecta. Solamente se aceptan letras del abecedario")
+            print()
+            continue #para que vuelva a "entrar" en el while para que pregunte otra letra ya que la ingresada no fue valida.
+
+        if letter in guessed:
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
+        else:
+            guessed.append(letter)
+            attempts -= 1
+            score -= 1
+            print("Esa letra no está en la palabra.")
+        print()
+    else:
+        print(f"¡Perdiste! La palabra era: {word}")
+        score = 0
 
 print(f"Puntaje final: {score}")
